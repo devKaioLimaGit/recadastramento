823bb7d249d8270158cc5b1721c3a1b7cc374bb9
Kaio Lima Pimentel <kaio.pimentel@paulista.local>
2025-06-06 09:51:39 -0300

    Atualizando

--- src/controllers/user/UpdateUsersController.js
+++ src/controllers/user/UpdateUsersController.js
@@ -59,7 +59,7 @@ class CreateUsersControllers {
                 id
             });
 
-            res.render("sucessupdate.ejs");
+            res.render("sucessUpdate.ejs");
         } catch (error) {
             res.status(400).json({ error: error.message });
 
